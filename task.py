b281b5337ae58b5e9df60aa28a873d29e87d405e
Vadim Danilov <99761501+vadimdanilov1@users.noreply.github.com>
2023-03-04 00:19:12 +0400

    Add files via upload

--- task.py
+++ task.py
@@ -1,8 +1,12 @@
+import random
+
 numbers1 = ['R', 'G', 'B', 'W']
 numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numbers3 = []
 order1 = []
 order2 = []
+cards = []
+players = []
 fin = True
 
 for i in range(len(numbers1)):
@@ -10,28 +14,50 @@ for i in range(len(numbers1)):
         numbers3.append(numbers1[i] + str(numbers2[j]))
 
 print('Введите команду "start N C" для раздачи N случайных карт, С игрокам')
-
+n = 0
+c = 0
 while fin:
     order1 = input().split()
     n = int(order1[1])
     c = int(order1[2])
 
-    if n * c > 40:
-      print('ВНИМАНИЕ! Карт в колоде всего 40 штук. Карт на всех игроков не хватит. Раздайте ещё раз.')
-    elif order1[0] != 'start':
+    if order1[0] != 'start':
         print('ВНИМАНИЕ! Команада "start" введена неверно . Введите команду ещё раз.')
+    elif n * c > 40:
+        print('ВНИМАНИЕ! Карт в колоде всего 40 штук. Карт на всех игроков не хватит. Раздайте ещё раз.')
     else:
         fin = False
 
-fin = True
-print('Введите команду "get-cards C" для раздачи карт игроку, где С - номер игрока')
+again = 'y'
+while again.lower() == 'y':
+    fin = True
+    print('Введите команду "get-cards C" для раздачи карт игроку, где С - номер игрока')
+    while fin:
+        order2 = input().split()
+        c_number = order2[1]
+        if order2[0] != 'get-cards' or len(order2) != 2:
+            print('ВНИМАНИЕ! Команада "get-cards" введена неверно . Введите команду ещё раз.')
+        elif int(c_number) > c:
+            print('ВНИМАНИЕ! Номер игрока больше, чем общее количество игроков. Введите команду еще раз.')
+        elif int(c_number) in players:
+            print('Этому игроку карты уже раздавались. Введите команду ещё раз.')
+        else:
+            players.append(int(c_number))
+            fin = False
 
-while fin:
-    order2 = input().split()
-    c_number = int(order2[1])
-    if c_number > c:
-        print('ВНИМАНИЕ! Номер игрока больше, чем общее количество игроков. Введите команду еще раз.')
-    elif order2[0] != 'get-cards':
-        print('ВНИМАНИЕ! Команада "get-cards" введена неверно . Введите команду ещё раз.')
-    else:
-        fin = False
+    cards = list()
+    for i in range(n):
+        cards.append(numbers3[random.randrange(len(numbers3))])
+        numbers3.remove(cards[i])
+
+    cards.insert(0, order2[1])
+    print(*cards)
+
+    if len(numbers3) < n:
+        print('Карт для раздачи больше не осталось')
+        break
+    again = input('Раздать ещё? (y = yes, n = no):  ')
+
+print('Карты розданы')
+print('Оставшиеся карты в колоде:', *numbers3)
+print('Количество оставшихся к колоде карт:', len(numbers3))
